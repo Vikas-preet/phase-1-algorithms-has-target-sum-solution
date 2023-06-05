3073ee849aa06807170cac660a6bd1065d7caf80
Vikas preet Tiwana <vikastiwana8@gmail.com>
2023-06-04 18:35:10 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {}
+  for (const number of array) {
+    const complement = target - number
+    if (seenNumbers[complement]) return true
+    seenNumbers[number] = true
+  }
+  return false
 }
-
 /* 
   Write the Big O time complexity of your function here
+  time complexity = O(n) and 
+  space complexity = O(n)
 */
 
 /* 
@@ -17,18 +24,18 @@ function hasTargetSum(array, target) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  console.log("Expecting: true")
+  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10))
 
-  console.log("");
+  console.log("")
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+  console.log("Expecting: true")
+  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25))
 
-  console.log("");
+  console.log("")
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+  console.log("Expecting: false")
+  console.log("=>", hasTargetSum([1, 2, 5], 4))
 }
 
-module.exports = hasTargetSum;
+module.exports = hasTargetSum
